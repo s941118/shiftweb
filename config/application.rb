3af0278cd2cdb42b7a55f95c3b2b1d73c8f3e1b5
hungmi <hungmihungmi@gmail.com>
2018-04-10 21:57:39 +0800

    add application setting

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Shiftweb
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = "zh-TW"
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   end
 end
