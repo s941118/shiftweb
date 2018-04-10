a7f59b4d3194eba4db80a06c5b6b0424425f44ee
hungmi <hungmihungmi@gmail.com>
2018-04-10 22:06:19 +0800

    remove app setting

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,5 @@ module Shiftweb
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = "zh-TW"
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   end
 end
