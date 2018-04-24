4059ded508b412e167b3bdf88e12a0da30d4aa4e
hungmi <hungmihungmi@gmail.com>
2018-04-24 12:04:53 +0800

    move loadSingleWork js next to initWorks() in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,7 +16,8 @@ var Works = Barba.BaseView.extend({
     onEnter: function () {},
     onEnterCompleted: function () {
     	preloaderTimeline();
-        initWorks();
+			initWorks();
+			loadSingleWork();
     },
     onLeave: function () {},
     onLeaveCompleted: function () {
@@ -550,6 +551,13 @@ function initWorks() {
 	// 	$('.inner-preview-box').css({'margin-left': (Math.abs($('.preview-wrapper').offset().left - 260) / $('.preview-wrapper').innerWidth()) * 100 + '%'});
 	// 	console.log()
 	// }, 100);	
+}
+function loadSingleWork() {
+	var work_id_match = window.location.href.match(/works\/(\d+)/)
+  if (work_id_match !== null && work_id_match[1] > 0) {
+  	console.log("loading work" + work_id_match[1])
+    $(".work-block[data-work-id='" + work_id_match[1] + "']").trigger("click")
+  }	
 }
 function initMembers() {
 	initGlobal();
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,10 +3,3 @@
 //= require anime.min
 //= require jquery.kinetic.min
 //= require app
-Barba.Dispatcher.on('transitionCompleted', function(currentStatus, oldStatus) {
-	var work_id_match = window.location.href.match(/works\/(\d+)/)
-  if (work_id_match !== null && work_id_match[1] > 0) {
-  	console.log("loading work" + work_id_match[1])
-    $(".work-block[data-work-id='" + work_id_match[1] + "']").trigger("click")
-  }
-});
